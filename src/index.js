597317c22e2e9caf7112a6c25455bbf28d277e6d
Fausto A. Guerrero <enjoythelive1@gmail.com>
2021-03-21 13:00:36 +0100

    Scheduled newsletter for 8am

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+const schedule = require('node-schedule');
 const restAPI = require('./rest-API');
+const sendNewsletter = require('./services/send-newsletter');
 
 const server = restAPI.listen(process.env.PORT || 8080, () => {
   console.log(`Serving on port ${server.address().port}`);
+  schedule.scheduleJob('00 00 08 * * * ', sendNewsletter);
 });
